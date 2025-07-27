4938f27c579e975327b4aad3fd41efeb615238aa
Daniel Lam <daniel.wingchi.lam@gmail.com>
2025-07-27 01:45:22 -0500

    feat: Connected LLM to Tools and Added Schema Test

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def main():
     clients = {}
 
     command, args = (
-        ("uv", ["run", "mcp_server.py"])
+        ("fastmcp", ["run", "mcp_server.py"])
         if os.getenv("USE_UV", "0") == "1"
         else ("python", ["mcp_server.py"])
     )
